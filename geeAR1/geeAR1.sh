d72b0547964d0347cdb597f5d49880ee1ec2c91e
lcolladotor <lcollado@jhsph.edu>
2013-12-14 18:00:01 -0500

    Minor tweaks

--- geeAR1/geeAR1.sh
+++ geeAR1/geeAR1.sh
@@ -3,9 +3,9 @@
 ## This script is for running on the cluster
 
 ## Usage:
-# sh template.sh PROJECTDIR RUNDIR CHRNUM RESULTSDIR
+# sh geeAR1.sh PROJECTDIR RUNDIR CHRNUM RESULTSDIR
 ## Example:
-# sh template.sh derHippo geeAR1 22 /home/bst/student/lcollado/756final_code/results
+# sh geeAR1.sh derHippo geeAR1 22 /home/bst/student/lcollado/756final_code/results
 
 # Directories
 PROJECTDIR=$1
@@ -37,6 +37,6 @@ mv ${WDIR}/${sname}.* ${RESULTSDIR}/${PROJECTDIR}/chr${CHRNUM}/${RUNDIR}/logs
 echo "**** Job ends ****"
 date
 EOF
-call="qsub -cwd -l jabba,mem_free=100G,h_vmem=15G,h_fsize=10G -pe local 20 -N ${sname} -m e .${sname}.sh"
+call="qsub -cwd -l stanley,mem_free=100G,h_vmem=15G,h_fsize=10G -pe local 20 -N ${sname} -m e .${sname}.sh"
 echo $call
 $call
